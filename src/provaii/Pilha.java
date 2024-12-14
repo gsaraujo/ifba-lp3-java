72fba8868edbd6f2c392334838ad8c7314f2acff
Gustavo <sasaeucomvoce@hotmail.com>
2024-12-14 14:24:01 -0300

    feat: reverse stack

--- src/provaii/Pilha.java
+++ src/provaii/Pilha.java
@@ -3,6 +3,7 @@ package provaii;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Pilha {
     private int[] pilha;
@@ -67,6 +68,34 @@ public class Pilha {
         queue.add(4);
 
         System.out.println(queue);
+    }
+
+    public static void inverterPilha(Stack<Integer> pilha) {
+        //Verifica se a pilha está vazia ou se ela tem apenas um elemento
+        if (pilha == null || pilha.size() <= 1) {
+            return; // Não há o que inverter, já é a resposta.
+        }
+
+        //Pilha auxiliar
+        Stack<Integer> pilhaAuxiliar = new Stack<>();
+
+        //Move todos os elementos da pilha original para a pilha auxiliar
+        while (!pilha.isEmpty()) {
+            int valor = pilha.pop();
+            pilhaAuxiliar.push(valor);
+            /*
+            Forma ótima: pilhaAuxiliar.push(pilha.pop());
+            * */
+        }
+
+        //Move de volta da pilha auxiliar para a pilha original
+        while (!pilhaAuxiliar.isEmpty()) {
+            int valor = pilhaAuxiliar.pop();
+            pilha.push(valor);
+            /*
+            * Forma ótima: pilha.push(pilhaAuxiliar.pop());
+            * */
+        }
     }
 
     public static void main(String[] args) {
