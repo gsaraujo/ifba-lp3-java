50f3cfbd09779336bc621ce3d7f1ebc94cb8a914
Gustavo <sasaeucomvoce@hotmail.com>
2025-11-22 17:31:55 -0300

    feat: improve encapsulation

--- src/encapsulamento/EncapsulamentoMain.java
+++ src/encapsulamento/EncapsulamentoMain.java
@@ -1,4 +1,24 @@
 package encapsulamento;
 
 public class EncapsulamentoMain {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("João",20);
+        Pessoa p2 = new Pessoa("Pedro",20);
+        Pessoa p3 = new Pessoa("Maria",20);
+        p1.sobrenome = "Silva";
+
+        Carro carro = new Carro("BMW","X5","Azul","aah-99",1999);
+        carro.ligar();
+        carro.desligar();
+        Moto moto = new Moto();
+        moto.ligar();
+        moto.aMoto();
+        moto.desligar();
+        moto.aMoto();
+        p1.setNome("José");//;nome = "José";
+        p1.cpf = "12345678912";
+
+        p1.aPessoa();
+        p1.cumprimentar();
+    }
 }
